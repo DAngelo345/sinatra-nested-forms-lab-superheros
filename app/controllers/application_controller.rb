ac425554efa96e238c028a1833a55b48ab453997
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-04-26 22:05:45 -0400

    commit tree d8af5db1e420249d52875dd1d57e8d4d7857e5ce

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,24 @@ class App < Sinatra::Base
     end
     
     post "/teams" do 
-    binding.pry
-        @teams = Team.new(params[:team])
-        params[:team][:name].each do |details|
-
-         
+        # @team = params[:team]
+        # # binding.pry
+        # @members = params[:team][:members]
+      
+        #     @members.each do |hero|
+        #         binding.pry
+        #         @heroname << hero[:name]
+        # end
+        # @members post "/teams" do 
+        @team = params[:team]
+        @members =[]
+        params[:team][:members].each do |details|
+            @members << details
         end
+        @members
+        # binding.pry
+
+       
        
         erb:super_hero
     end
